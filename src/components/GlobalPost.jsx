111a2023e6a6f7ae95608b733d6bdded42bbf118
Josh Allison <jralli03@louisville.edu>
2023-11-28 21:49:57 -0700

    posts on the main page (not correct posts at the moment)

---
+++ src/components/GlobalPost.jsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+function GlobalPost({ post }) {
+    const [likes, setLikes] = useState(post.likeCount);
+
+    const handleLike = async () => {
+        const newLikeCount = likes + 1;
+        try {
+            const response = await axios.patch(`/api/post/${post.postId}`, {
+                likeCount: newLikeCount,
+            });
+
+            setLikes(response.data.likeCount);
+        } catch (error) {
+            console.error('Error liking post', error);
+        }
+    };
+
+
+    return (
+        <div className="relative mx-auto w-full flex h-20 flex-row border border-b justify-between">
+            <h1>{post.username}</h1>
+
+            <div className="flex flex-grow mx-6 p-2 max-h-[100px] overflow-y-auto" style={{ maxHeight: "100px", overflowY: "auto" }}>
+                <p className="text-lg break-words text-ellipsis">{post.content}</p>
+            </div>
+
+            <div className="flex flex-col">
+            
+            {/* try to get the date */}
+            <span className="text-xs p-2 text-slate-600">
+                {post.timestamp} {/* NOT WORKING CURRENTLY; just a placeholder | note: toLocaleDateString for just mm/dd/yy i.e. 11/28/23 */}
+            </span>
+
+            <button className="flex" onClick={handleLike}>
+                <img
+                    src="../src/assets/thumbs-up.png"
+                    alt="Thumbs Up"
+                    className="h-6 w-6 mx-4"
+                />
+                <span>{likes}</span> {/* Display the local likes state */}
+            </button>
+
+            </div>
+        </div>
+    );
+}
+
+export default GlobalPost;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import LeftColumn from '../components/LeftColumn.jsx';
 import RightColumn from '../components/RightColumn.jsx';
+import GlobalPosts from "../components/GlobalPosts.jsx";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
 function Main() {
+  const [posts, setPosts] = useState([]);
+  const token = localStorage.getItem('token');
+  const decodedToken = jwtDecode(token);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+        if (!decodedToken || !decodedToken.id) {
+            console.error('Invalid token or user ID not found');
+            return;
+        }
+
+        try {
+            const response = await axios('api/post/feed/' + decodedToken.id);
+            setPosts(response.data);
+        } catch (error) {
+            console.error('Error fetching posts:', error);
+        }
+    };
+    fetchPosts();
+}, []);
 
   return (
     <div className="flex h-screen">
@@ -12,6 +35,9 @@ function Main() {
       <div className="h-screen w-1/2 border-2"> { /* middle column */ }
         <div className="w-full py-4 border-b-2 text-4xl text-center font-semibold">
           <h1 className="text-slate-700">Main</h1>
+        </div>
+        <div className="h-full w-full border-2 flex flex-col justify-center items-center no-scrollbar overflow-y-scroll">
+          <GlobalPosts posts={posts}/>
         </div>
       </div> { /* end of middle column */ }
       <div className="h-screen w-1/4"> { /* third column */ }
